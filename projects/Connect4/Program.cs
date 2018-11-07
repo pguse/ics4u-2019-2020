46ee24b831475f8f7d6a433b841fadb0538db090
pguse <pguse@albertcollege.ca>
2018-11-07 11:38:36 -0500

    added tictactoe and connect4

---
+++ projects/Connect4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Connect4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] pos = { {'-','-','-','-','-','-','-'},
+                            {'-','-','-','-','-','-','-'},
+                            {'-','-','-','-','-','-','-'},
+                            {'-','-','-','-','-','-','-'},
+                            {'-','-','-','-','-','-','-'},
+                            {'-','-','-','-','-','-','-'}};
+
+            char player = 'X';
+            bool over = false;
+            while (!over){
+                Console.Clear();
+                Draw(pos);
+                player = Update(player,pos);
+                over = GameOver(pos);
+            }
+        }
+        
+        static char Update(char piece, char[,] p){
+            Console.Write("Where do you want to play (1-7)? ");
+            int loc = int.Parse(Console.ReadLine());
+            // Checks column specified by loc for the first open
+            // space closest the bottom
+            for(var i=5; i >= 0; i--){
+                if (p[i, loc-1] == '-'){
+                    p[i, loc-1] = piece;
+                    break;
+                }
+            }
+            if (piece == 'X'){
+                return 'O';
+            }
+            else{
+                return 'X';
+            }
+        }
+        
+        static bool GameOver(char[,] p){
+            // Modify this function so that a
+            // win or tie is detected and then
+            // the approprate message is written
+            // to the console.  e.g. X wins! or Tie!
+            return false;
+        }
+        static void Draw(char[,] p){
+            string row = "| {0} | {1} | {2} | {3} | {4} | {5} | {6} |";
+            string bottom = "|---|---|---|---|---|---|---|\n";
+            for(var i=0; i < 6; i++){
+                Console.WriteLine(row,p[i,0],p[i,1],p[i,2],p[i,3],p[i,4],p[i,5],p[i,6]);
+            }
+            Console.WriteLine(bottom);
+        }
+    }
+}
---
+++ projects/TicTacToe/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] pos = {'-','-','-','-','-','-','-','-','-'};
+            char player = 'X';
+            bool over = false;
+            while (!over){
+                Console.Clear();
+                Draw(pos);
+                player = Update(player,pos);
+                over = GameOver(pos);
+            }
+        }
+        
+        static char Update(char piece, char[] p){
+            Console.Write("Where do you want to play? ");
+            int loc = int.Parse(Console.ReadLine());
+            p[loc-1] = piece;
+            if (piece == 'X'){
+                return 'O';
+            }
+            else{
+                return 'X';
+            }
+        }
+        
+        static bool GameOver(char[] p){
+            // Modify this function so that a
+            // win or tie is detected and then
+            // the approprate message is written
+            // to the console.  e.g. X wins! or Tie!
+            return false;
+        }
+        static void Draw(char[] p){
+            string row0 = "  {0}  |  {1}  |  {2}  \n";
+            string row1 = "  {3}  |  {4}  |  {5}  \n";
+            string row2 = "  {6}  |  {7}  |  {8}  \n";
+            string line = "-----|-----|-----\n";
+            string board = row0 + line + row1 + line + row2;
+            Console.WriteLine(board,p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]);
+        }
+    }
+}
